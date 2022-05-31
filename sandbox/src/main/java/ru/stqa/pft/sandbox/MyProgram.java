2b6810cfd721d340fd1e669b6068ca6b164cc521
VladimirMitienko <strykerjt@gmail.com>
2022-05-31 12:00:19 +0700

    Создан метод вычисляющий расстояние между двумя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
@@ -2,6 +2,13 @@ package ru.stqa.pft.sandbox;
 
 public class MyProgram {
     public static void main(String[] args) {
-
+        Point p1 = new Point();
+        Point p2 = new Point();
+        p1.p1 = 3.4;
+        p2.p2 = 5.6;
+        System.out.println("Расстояние между двумя точками равна: " + distance(p1, p2));
     }
+    public static double distance(Point p1, Point p2) {
+            return Math.sqrt(p1.p1 + p2.p2);
+        }
 }
